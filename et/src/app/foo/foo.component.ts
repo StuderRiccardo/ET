736d8266fa7fba1fe7987dc21656e014c67f87f5
StuderRiccardo <studer.riccardo@iisgalvanimi.edu.it>
2023-04-26 12:49:11 +0000

    finito

--- et/src/app/foo/foo.component.ts
+++ et/src/app/foo/foo.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -6,9 +7,37 @@ import { Observable } from 'rxjs';
   templateUrl: './foo.component.html',
   styleUrls: ['./foo.component.css']
 })
-export class FooComponent implements OnInit{
-  data: object;
-  loading: boolean;
-  o : Observable<object>;
+export class FooComponent {
+  data!: Object;
+  loading!: boolean;
+  o !:Observable<Object>;
+  constructor(public http: HttpClient) {}
+  makeRequest(): void {
+    console.log("here");
+    this.loading = true;
+    this.o = this.http.get('https://jsonplaceholder.typicode.com/posts/1');
+    this.o.subscribe(this.getData);
+  }
+  getData = (d : Object) =>
+  {
+    this.data = new Object(d);
+    this.loading = false;
+  }
+
+  makeCompactPost(): void{
+    this.loading = true;
+    this.http
+      .post('https://jsonplaceholder.typicode.com/posts',
+        JSON.stringify({
+          body: 'bar',
+          title: 'foo',
+          userId: 1
+        })
+        )
+        .subscribe(data => {
+          this.data = data;
+          this.loading = false;
+        });
+  }
 
 }
